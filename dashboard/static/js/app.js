915ef1b0c884e6a59d3fd4ed1e1ab91642ab1bff
rickystuart1 <rickystuart6@yahoo.com>
2022-07-20 19:14:59 -0500

    adding updated dashboard

--- dashboard/static/js/app.js
+++ dashboard/static/js/app.js
@@ -5,3 +5,4 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
---
+++ dashboard/static/js/table.js
@@ -0,0 +1,9 @@
+// for 10 player page want single table with filter button maybe that (drop down?)
+// an event listener that displays the running backs or qbs etc and have a line graph or ppgame 
+// with a plotly graph (find what kind of visualization)
+// graph showing averages and how the top 10 come up against that 
+
+
+// top 200 one table 
+
+//check out http://tabulator.info/
