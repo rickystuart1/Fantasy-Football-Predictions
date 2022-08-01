0b93b43304d06cbf0bbd33865c4e36f02fd51399
rickystuart1 <rickystuart6@yahoo.com>
2022-07-31 23:25:29 -0500

    adding dashboard files

--- dashboard/static/js/app.js
+++ dashboard/static/js/app.js
@@ -1,8 +1,15 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const filterbtn = document.querySelectorAll('.filter-btn')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+filterbtn.forEach(btn => btn.addEventListener('click', function(event) {
+    event.preventDefault()
+    event.target.classList.toggle('is-active')
+    filterTable({ value: event.target.id, key: 'position'})
+}))
 
